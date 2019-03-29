7d0e0571095411d0100bb51ff586f87210511f8e
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-29 14:56:02 -0400

    add CRUD operations to Todo

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import '../styles/Todo.css'
 
 const fakeData = [{
   id: '1',
@@ -27,41 +28,104 @@ export default class Todo extends Component {
     super(props)
     this.state = {
       edit: false,
-    id: null,
-    data: fakeData
+      id: null,
+      data: fakeData
     }
   }
 
-  onSubmitHandle() {
+  // create
+  onSubmitHandle(e) {
+    e.preventDefault()
 
+    this.setState({
+      data: [...this.state.data, {
+        id: Date.now(),
+        title: e.target.item.value,
+        done: false,
+        date: new Date()
+      }]
+    })
+
+    e.target.item.value = ''
   }
 
-  onDeleteHandle() {
-
+  // update
+  onEditHandle(e) {
+    this.setState({
+      edit: true,
+      id: arguments[0],
+      title: arguments[1]
+    })
   }
 
-  onEditHandle() {
+  onUpdateHandle(e) {
+    e.preventDefault()
 
+    this.setState({
+      data: this.state.data.map(item => {
+        if (item.id === this.state.id) {
+          item['title'] = e.target.updatedItem.value
+          return item
+        }
+        return item
+      }),
+      edit: false
+    })
   }
 
-  onCompleteHandle() {
+  onCompleteHandle(e) {
+    let id = arguments[0]
 
+    this.setState({
+      data: this.state.data.map(item => {
+        if (item.id === id) {
+          item['done'] = true
+          return item
+        }
+        return item
+      })
+    })
+  }
+
+  // delete
+  onDeleteHandle(e) {
+    let id = arguments[0]
+    console.log(arguments)
+
+    this.setState({
+      data: this.state.data.filter(item => item.id !== id)
+    })
+  }
+
+  renderEditForm() {
+    if (this.state.edit) {
+      return (
+        <form onSubmit={this.onUpdateHandle.bind(this)}>
+          <input type="text" name="updatedItem" className="item" defaultValue={this.state.title} />
+          <button className="update-add-item">Update</button>
+        </form>
+      )
+    }
   }
 
   render() {
     return (
       <div>
+
+      {this.renderEditForm()}
+
       <form onSubmit={this.onSubmitHandle.bind(this)}>
         <input type="text" name="item" className="item" />
         <button className="btn-add-item">Add</button>
       </form>
+
       <ul>
         {this.state.data.map(item => (
-          <li key={item.id}>
+          <li className={ item.done ? 'done' : 'hidden' } key={item.id}>
             {item.title}
             <button onClick={this.onDeleteHandle.bind(this, item.id)}>Delete</button>
             <button onClick={this.onEditHandle.bind(this, item.id, item.title)}>Edit</button>
-            <button onClick={this.onCompleteHandle}>Complete</button>
+            <button onClick={this.onCompleteHandle.bind(this, item.id)}>Complete</button>
           </li>
         ))}
       </ul>
