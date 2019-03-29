6ed5f92350975742d1222923010030ef1cbb3933
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-29 17:09:39 -0400

    added callDB method to client app

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import {Todo} from './components';
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {apiResponse: ''}
+    this.state = {
+      apiResponse: '',
+      dbResponse: ''
+    }
   }
 
   async callAPI() {
@@ -20,12 +23,17 @@ class App extends Component {
     }
   }
 
-  // callAPI() {
-  //   fetch('http://localhost:9000/testAPI')
-  //   .then(res => res.text())
-  //   .then(res => this.setState({ apiResponse: res }))
-  //   .catch(err => err)
-  // }
+  async callDB() {
+    try {
+      const res = await fetch('http://localhost:9000/testDB')
+      const text = await res.text()
+      this.setState({
+        dbResponse: text
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   componentWillMount() {
     this.callAPI()
