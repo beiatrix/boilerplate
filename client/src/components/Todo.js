887a00f53316a4a08a687e337213ec4ff2f095ff
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-29 13:43:03 -0400

    add fake data to Todo class

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,6 +1,36 @@
 import React, {Component} from 'react'
 
+const fakeData = [{
+  id: '1',
+  title: 'Buy Milk',
+  done: false,
+  date: new Date()
+}, {
+  id: '2',
+  title: 'Meeting with Ali',
+  done: false,
+  date: new Date()
+}, {
+  id: '3',
+  title: 'Tea break',
+  done: false,
+  date: new Date()
+}, {
+  id: '4',
+  title: 'Go for a run.',
+  done: false,
+  date: new Date()
+}]
+
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      edit: false,
+    id: null,
+    data: fakeData
+    }
+  }
   render() {
     return (
       <h1>hiya</h1>
